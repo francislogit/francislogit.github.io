a8e717066615b0b47c08eb32deb5ef4500a9ae16
Francis Lo <francislowork@gmail.com>
2020-08-29 13:44:40 +0800

    Let code be more readable ( refine paging part )

--- js/sidebar.js
+++ js/sidebar.js
@@ -25,26 +25,32 @@ function genSidebar(data){
         if(isLevel.length > 1){
             $("#sidebar").append("<div id='sidebar-level" + isLevel[1] + "' class='sidebar-level'></div>");
             if(parseInt(isLevel[1]) > 1){
-                str += "<span id='goBack" + isLevel[1] + "' class='back-item'>Go Back</span>"
+                $("#sidebar-level" + isLevel[1]).append("<span id='goBack" + isLevel[1] + "' class='back-item'>Go Back</span>");
+                $("#goBack" + isLevel[1]).data("prevLevel", parseInt(isLevel[1])-1);
             }
-            str += "<ul>";
+            $("#sidebar-level" + isLevel[1]).append("<ul></ul>");
             $.each(data,function(index,side_data){
                 if(parseInt(isLevel[1]) > 1){
                     $.each(side_data,function(index2,side2_data){
-                        str += sidebarTemplate(side2_data, index);
+                        str = sidebarTemplate(side2_data, index);
+                        str.data("belongTo", index);
+                        str.data("currentLevel",parseInt(isLevel[1]));
+                        $("#sidebar-level" + isLevel[1] + " ul").append(str);
                     });
                 }else{
-                    str += sidebarTemplate(side_data);
+                    str = sidebarTemplate(side_data);
+                    str.data("belongTo", index);
+                    str.data("currentLevel",parseInt(isLevel[1]));
+                    $("#sidebar-level" + isLevel[1] + " ul").append(str);
                 }
             });
-            str += "</ul>";
-            $("#sidebar-level" + isLevel[1]).html(str);
         }
     });
     sideEvt();
 }
 function sidebarTemplate(data, index){
     var str = "";
+    var $str = "";
     var hasSubStr = "";
     var secTarget = "";
     if(index !== undefined){
@@ -56,22 +62,18 @@ function sidebarTemplate(data, index){
     }else{
         hasSubStr += "<div class='sidebar-text'>" + data.tag + "</div>";
     }
-    if(data.link !== undefined){
-        if(secTarget.length == 0){
-            secTarget = data.link.split(".")[1];
-        }
-        str += "<li id='" + data.id + "' class='side-item " + data.class + "' target-link='" + data.link + "' obj-title='" + secTarget + "'>" + hasSubStr + "</li>";
-    }else{
-        str += "<li id='" + data.id + "' class='side-item " + data.class + "' target-url='" + data.url + "' obj-title='" + secTarget + "'>" + hasSubStr + "</li>";
-    }
-    
-    return str;
+    str += "<li id='" + data.id + "' class='side-item " + data.class + "'>" + hasSubStr + "</li>";
+    $str = $(str);
+    $str.data("currentTag", data.tag)
+    $str.data("followWith", data.link );
+    $str.data("endWith", data.url );
+    return $str;
 }
 function sideEvt(){
     $(".side-item").on("click",function(){
-        var targetLink = $(this).attr("target-link");
-        var targetUrl = $(this).attr("target-url");
-        if(targetLink !== undefined){
+        var targetLink = $(this).data("followWith");
+        var targetUrl = $(this).data("endWith");
+        if(targetLink !== undefined && targetLink.length != 0){
             var level = parseInt(targetLink.split(".")[0].split("level")[1]);
             var tarObj = targetLink.split(".")[1];
             sidebarSwitch((level-1), level, tarObj);
@@ -79,7 +81,7 @@ function sideEvt(){
                 $("#page-url .stop").remove();
             }
             $("#page-url").append("<span> > " + $(this).find(".sidebar-text").text() + "</span>");
-        }else if(targetUrl !== undefined){
+        }else if(targetUrl !== undefined && targetUrl.length != 0){
             $("#inside-cont").load("inside_page/" + targetUrl);
             if($("#page-url .stop").length == 0){
                 $("#page-url").append("<span class='stop'> > " + $(this).find(".sidebar-text").text() + "</span>");
@@ -102,7 +104,11 @@ function sidebarSwitch(currentLevel, targetLevel, targetObj){
     }else{
         $("#sidebar-level" + currentLevel).hide();
         $("#sidebar-level" + targetLevel).find(".side-item").hide();
-        $("#sidebar-level" + targetLevel + " li[obj-title='" + targetObj + "']").show();
+        $("#sidebar-level" + targetLevel + " li").each(function(){
+            if(targetObj == $(this).data("belongTo")){
+                $(this).show();
+            }
+        })
         $("#sidebar-level" + targetLevel).show();
     }
 }
